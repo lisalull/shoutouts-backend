88d3e25614c0537a6f3c4ebb2d0bb0faf3f7b10b
lisa <lisa.marie.lull@gmail.com>
2021-11-29 09:12:48 -0500

    initial commit

---
+++ functions/src/models/ShoutOut.ts
@@ -0,0 +1,10 @@
+import { ObjectId } from "mongodb";
+
+export default interface ShoutOut {
+  _id?: ObjectId;
+  to: string;
+  from: string;
+  message: string;
+  photoURL?: string;
+  image?: string;
+}
